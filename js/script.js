92bf28f28cbd3b4ed5db8a8d5ca594cae03fe2bf
Jordan Jones <jordanalanjones@gmail.com>
2015-12-28 23:48:32 -0600

    Testing on back end

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
   $(".foot").load("partials/footer.html");
   $(".navpar").load("partials/nav.html");
+  $("input[type=submit]").click(function(){
+    $(".success").show();
+  });
 });
