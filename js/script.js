f6fca0194c6dbd4e8e260238c6d865c88ff617d6
Jordan Jones <jordanalanjones@gmail.com>
2015-12-28 20:46:48 -0600

    May write to a file

--- js/script.js
+++ js/script.js
@@ -2,3 +2,20 @@ $(document).ready(function(){
       $(".foot").load("partials/footer.html");
       $(".navpar").load("partials/nav.html");
 });
+$(document).ready(function(){
+  $("form[name=res]").submit(function(event) {
+
+     /* stop form from submitting normally */
+     event.preventDefault();
+
+     /* get some values from elements on the page: */
+     var $form = $(this),
+         term = $form.find('input[name="name"]').val(),
+         url = $form.attr('action');
+
+     /* Send the data using post */
+     var posting = $.post("reservation/reservations.asp", {
+         s: term
+     });
+ });
+})
