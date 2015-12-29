fc12640241a88b8d8f8f8f1690782b1243166a43
Jordan Jones <jordanalanjones@gmail.com>
2015-12-29 09:40:05 -0600

    Testing

--- js/script.js
+++ js/script.js
@@ -4,4 +4,25 @@ $(document).ready(function(){
   $("input[type=button]").click(function(){
     $(".ressuccess").fadeIn();
   });
+  $(function() {
+  $(window).scroll( function(){
+
+
+    $('.navscroll').each( function(i){
+
+        var bottom_of_object = $(this).position().top + $(this).outerHeight();
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+        /* Adjust the "200" to either have a delay or that the content starts fading a bit before you reach it  */
+        bottom_of_window = bottom_of_window + 200;
+
+        if( bottom_of_window > bottom_of_object ){
+
+            $(this).animate({'opacity':'1'},500);
+
+        }
+    });
+
+  });
+  });
 });
