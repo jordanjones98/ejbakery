3b674e711007cf3e3c82151657f992ce40131f1c
Jordan Jones <jordanalanjones@gmail.com>
2015-12-28 23:45:57 -0600

    Testing server side

--- js/script.js
+++ js/script.js
@@ -1,21 +1,9 @@
 $(document).ready(function(){
-      $(".foot").load("partials/footer.html");
-      $(".navpar").load("partials/nav.html");
+  $(".foot").load("partials/footer.html");
+  $(".navpar").load("partials/nav.html");
 });
-$(document).ready(function(){
-  $("form[name=res]").submit(function(event) {
-
-     /* stop form from submitting normally */
-     event.preventDefault();
-
-     /* get some values from elements on the page: */
-     var $form = $(this),
-         term = $form.find('input[name="name"]').val(),
-         url = $form.attr('action');
-
-     /* Send the data using post */
-     var posting = $.post("reservation/reservations.txt", {
-         s: term
-     });
- });
-})
+%(document).ready(function(){
+  %("input[type=submit]").click(function(){
+    $(".success").show();
+  });
+});
