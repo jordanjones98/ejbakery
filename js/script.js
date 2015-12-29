3f421a38a5d016c2fb8a03b98af4b80a01dae802
Jordan Jones <jordanalanjones@gmail.com>
2015-12-29 01:00:29 -0600

    Most is working pushing for Elijah

--- js/script.js
+++ js/script.js
@@ -2,11 +2,6 @@ $(document).ready(function(){
   $(".foot").load("partials/footer.html");
   $(".navpar").load("partials/nav.html");
   $("input[type=button]").click(function(){
-    if( !$(this).val() ) {
-          $(".success").fadeIn("slow");
-    }
-    else if ($(this).val()) {
-    $(".error").fadein("slow");
-    }
+    $(".ressuccess").fadeIn();
   });
 });
