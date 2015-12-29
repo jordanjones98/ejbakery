02f6c7879872f2420f8d09d0faab0cd0d4fa24bf
Jordan Jones <jordanalanjones@gmail.com>
2015-12-29 09:46:16 -0600

    Testing

--- js/script.js
+++ js/script.js
@@ -7,9 +7,4 @@ $(document).ready(function(){
 });
 $(window).on('scroll', function() {
     var header = $(".navscroll");
-    if ($(this).scrollTop() > 50) {
-        if (!header.data('faded')) header.data('faded', 1).stop(true).fadeTo(400, 0.5);
-    } else if (header.data('faded')) {
-        header.data('faded', 0).stop(true).fadeTo(400, 1);
-    }
 });
