c560e81f3dd844f6c92f609afdf168ace7c18978
Jordan Jones <jordanalanjones@gmail.com>
2015-12-29 09:44:23 -0600

    Testing more jQuery

--- js/script.js
+++ js/script.js
@@ -5,24 +5,11 @@ $(document).ready(function(){
     $(".ressuccess").fadeIn();
   });
 });
-$(function() {
-    $(window).scroll( function(){
-
-
-        $('.navscroll').each( function(i){
-
-            var bottom_of_object = $(this).position().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-
-            /* Adjust the "200" to either have a delay or that the content starts fading a bit before you reach it  */
-            bottom_of_window = bottom_of_window + 200;
-
-            if( bottom_of_window > bottom_of_object ){
-
-                $(this).animate({'opacity':'1'},500);
-
-            }
-        });
-
-    });
+$(window).on('scroll', function() {
+    var header = $(".navscroll");
+    if ($(this).scrollTop() > 50) {
+        if (!header.data('faded')) header.data('faded', 1).stop(true).fadeTo(400, 0.5);
+    } else if (header.data('faded')) {
+        header.data('faded', 0).stop(true).fadeTo(400, 1);
+    }
 });
