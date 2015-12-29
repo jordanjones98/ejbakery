4c6d57c56a6d6dd7810d6d14276e1a79154af2e6
Jordan Jones <jordanalanjones@gmail.com>
2015-12-28 23:52:05 -0600

    Cant type

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $(".foot").load("partials/footer.html");
   $(".navpar").load("partials/nav.html");
-  $("input[type=s=button]").click(function(){
+  $("input[type=button]").click(function(){
     $(".success").show();
   });
 });
