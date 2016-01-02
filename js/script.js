5b6ca31a85a031925ddae42beb224741dc564f18
Jordan Jones <jordanalanjones@gmail.com>
2016-01-01 18:06:19 -0600

    Many Changes

--- js/script.js
+++ js/script.js
@@ -4,12 +4,4 @@ $(document).ready(function(){
   $("input[type=button]").click(function(){
     $(".ressuccess").fadeIn();
   });
-  // call waypoint plugin
-  $("navscroll").waypoint(function(event, direction) {
-      // do your fade in here
-      $(this).fadeIn();
-  },
-  offset: 10
-
-  });
 });
