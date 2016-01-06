55d9db92e684cc8c819a4a6b0c14e24e0d7925a2
Jordan Jones <jordanalanjones@gmail.com>
2016-01-06 14:50:17 -0600

    Testing Hamburger Menu

--- js/script.js
+++ js/script.js
@@ -13,3 +13,20 @@ function test(){
     var userInput = document.getElementById("userInput").value;
     document.getElementById("ressuccess").innerHTML = userInput;
 }
+
+// HAMBURGER MENU
+$( ".cross" ).hide();
+$( ".menu" ).hide();
+$( ".hamburger" ).click(function() {
+$( ".menu" ).slideToggle( "slow", function() {
+$( ".hamburger" ).hide();
+$( ".cross" ).show();
+});
+});
+
+$( ".cross" ).click(function() {
+$( ".menu" ).slideToggle( "slow", function() {
+$( ".cross" ).hide();
+$( ".hamburger" ).show();
+});
+});
