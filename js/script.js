6f6114d36d9f6fc48fd417fb1b859962ae39bf85
Jordan Jones <jordanalanjones@gmail.com>
2016-01-07 06:39:10 -0600

    Took out old hamburger

--- js/script.js
+++ js/script.js
@@ -13,20 +13,3 @@ function test(){
     var userInput = document.getElementById("userInput").value;
     document.getElementById("ressuccess").innerHTML = userInput;
 }
-
-// HAMBURGER MENU
-$( ".cross" ).hide();
-$( ".menu" ).hide();
-$( ".hamburger" ).click(function() {
-$( ".menu" ).slideToggle( "slow", function() {
-$( ".hamburger" ).hide();
-$( ".cross" ).show();
-});
-});
-
-$( ".cross" ).click(function() {
-$( ".menu" ).slideToggle( "slow", function() {
-$( ".cross" ).hide();
-$( ".hamburger" ).show();
-});
-});
