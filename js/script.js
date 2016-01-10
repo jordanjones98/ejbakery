62aab87c9a89216331c25a3c70c18dcafa1ba783
Jordan Jones <jordanalanjones@gmail.com>
2016-01-09 19:26:43 -0600

    Mostly navmobile

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 $(document).ready(function(){
   $(".foot").load("partials/footer.html");
   $(".navpar").load("partials/nav.html");
+  $(".navmobile").load("partials/mobilenav.html");
   $("input[type=button]").click(function(){
     $(".ressuccess").fadeIn();
   });
