2f92c5a232cd81c6132c727a4cdbb92c5b8363e1
Jordan Jones <jordanalanjones@gmail.com>
2016-01-12 23:02:58 -0600

    Testing some changes

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 // JQUERY
+var about = "about.html"
 $(document).ready(function(){
   $(".foot").load("partials/footer.html");
   $(".navpar").load("partials/nav.html");
   $(".navmobile").load("partials/mobilenav.html");
   $("input[type=button]").click(function(){
     $(".ressuccess").fadeIn();
+  $('.navmobile').toggle(function(){
+    $('hammenu').fadeIn
+  });
+
   });
 });
 
